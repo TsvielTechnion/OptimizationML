3a1c6883679568e1435dc13a2a800d35cbe83c89
tsviel <tsviel12@campus.technion.ac.il>
2020-05-21 20:12:13 +0300

    finished main

--- HW1.py
+++ HW1.py
@@ -1,39 +1,106 @@
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
-
+import pandas as pd
 from matrix import GenData
 from algorithms import NonSmoothPGD, SmoothGD, AcceleratedGD
 
-
 """
-    Starting With Positive Definite A.T * A
+Constant Parameters
 """
-A_sigma_min = 0
-A_sigma_max = 5
+sb.set()
 columns = 10
 rows = 100
-
-optimal_solution = np.random.randint(low=1, high=5, size=10)
-R = np.linalg.norm(optimal_solution, ord=2)
-gen = GenData(sigma_min=A_sigma_min, sigma_max=A_sigma_max, columns=columns, rows=rows, optimal_sol=optimal_solution)
-A, b = gen.gen_data()
-
 x_1 = np.zeros(columns)
 epsilon = 0.01
 
 """
-Running the algorithm
+    Starting With Positive Definite A.T * A
 """
-# non_smooth_pgd = NonSmoothPGD(A, b, x_1, epsilon, R, A_sigma_max)
-# non_smooth_pgd_result = non_smooth_pgd.run()
+A_sigma_min = 1
+A_sigma_max = 10
 
-# smooth_gd = SmoothGD(A, b, x_1, epsilon, A_sigma_max**2)
-# smooth_gd_result = smooth_gd.run()
+
+optimal_solution = np.random.randint(low=A_sigma_min, high=A_sigma_max, size=columns)
+R = np.linalg.norm(optimal_solution, ord=2)
+gen = GenData(sigma_min=A_sigma_min, sigma_max=A_sigma_max, columns=columns, rows=rows, optimal_sol=optimal_solution)
+A, b = gen.gen_data()
+
+"""
+Running the algorithms
+"""
+
+non_smooth_pgd = NonSmoothPGD(A, b, x_1, epsilon, R, A_sigma_max)
+non_smooth_pgd_result = non_smooth_pgd.run()
+non_smooth_pgd_result['iterations'] = [i for i in range(len(non_smooth_pgd_result['x']))]
+df_non_smooth_pgd = pd.DataFrame(non_smooth_pgd_result)
+df_non_smooth_pgd["algorithm"] = "Non Smooth PGD"
+
+smooth_gd = SmoothGD(A, b, x_1, epsilon, A_sigma_max**2)
+smooth_gd_result = smooth_gd.run()
+smooth_gd_result['iterations'] = [i for i in range(len(smooth_gd_result['x']))]
+df_smooth_gd = pd.DataFrame(smooth_gd_result)
+df_smooth_gd["algorithm"] = "Smooth GD"
+
 
 accelerated_gd = AcceleratedGD(A, b, x_1, epsilon, A_sigma_max**2, A_sigma_min**2)
 accelerated_gd_result = accelerated_gd.run()
+accelerated_gd_result['iterations'] = [i for i in range(len(accelerated_gd_result['x']))]
+df_accelerated_gd = pd.DataFrame(accelerated_gd_result)
+df_accelerated_gd["algorithm"] = "Accelerated GD"
 
-sb.lineplot(x=[i for i in range(len(accelerated_gd_result['x']))],
-            y=accelerated_gd_result['fx'])
+results = pd.concat([df_smooth_gd, df_non_smooth_pgd, df_accelerated_gd])
+
+f, ax = plt.subplots(figsize=(7, 7))
+ax.set(xscale="log", yscale="log")
+sb.lineplot(x=results['iterations'],
+            y=results['fx'],
+            hue=results['algorithm'],
+            ax=ax,
+            )
 plt.show()
+
+"""
+Not Positive Definite A.T * A
+"""
+# A_sigma_min = 0
+# A_sigma_max = 5
+#
+# optimal_solution = np.random.randint(low=A_sigma_min, high=A_sigma_max, size=columns)
+# R = np.linalg.norm(optimal_solution, ord=2)
+# gen = GenData(sigma_min=A_sigma_min, sigma_max=A_sigma_max, columns=columns, rows=rows, optimal_sol=optimal_solution)
+# A, b = gen.gen_data()
+
+
+"""
+Running the algorithms
+"""
+# non_smooth_pgd = NonSmoothPGD(A, b, x_1, epsilon, R, A_sigma_max)
+# non_smooth_pgd_result = non_smooth_pgd.run()
+# non_smooth_pgd_result['iterations'] = [i for i in range(len(non_smooth_pgd_result['x']))]
+# df_non_smooth_pgd = pd.DataFrame(non_smooth_pgd_result)
+# df_non_smooth_pgd["algorithm"] = "Non Smooth PGD"
+#
+# smooth_gd = SmoothGD(A, b, x_1, epsilon, A_sigma_max**2)
+# smooth_gd_result = smooth_gd.run()
+# smooth_gd_result['iterations'] = [i for i in range(len(smooth_gd_result['x']))]
+# df_smooth_gd = pd.DataFrame(smooth_gd_result)
+# df_smooth_gd["algorithm"] = "Smooth GD"
+#
+#
+# accelerated_gd = AcceleratedGD(A, b, x_1, epsilon, A_sigma_max**2, A_sigma_min**2)
+# accelerated_gd_result = accelerated_gd.run()
+# accelerated_gd_result['iterations'] = [i for i in range(len(accelerated_gd_result['x']))]
+# df_accelerated_gd = pd.DataFrame(accelerated_gd_result)
+# df_accelerated_gd["algorithm"] = "Accelerated GD"
+#
+# results = pd.concat([df_smooth_gd, df_non_smooth_pgd, df_accelerated_gd])
+#
+# f, ax = plt.subplots(figsize=(7, 7))
+# ax.set(xscale="log", yscale="log")
+# sb.lineplot(x=results['iterations'],
+#             y=results['fx'],
+#             hue=results['algorithm'],
+#             ax=ax,
+#             )
+# plt.show()
