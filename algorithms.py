8e291e5ce3f817435eb47e6decab6db4d1c9ee53
noamarbe <noam.arbel@intel.com>
2020-05-21 16:53:34 +0300

    implemented algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,22 +1,24 @@
 import numpy as np
 from abc import abstractmethod
+from copy import deepcopy
 
 
 class GradientDecent:
-    def __init__(self, A: np.ndarray, b: np.array, x_0: np.array, epsilon: float):
+    def __init__(self, A: np.ndarray, b: np.array, x_1: np.array, epsilon: float):
         self.A = A
         self.b = b
-        self.x_0 = x_0
+        self.x_1 = x_1
         self.epsilon = epsilon  # stopping condition
 
-    def run(self, x, t=0):
+    def run(self, x, t=1):
         next_x = self.compute_next_x(x, t)
         fx = self.zero_order_oracle(x)
         f_next_x = self.zero_order_oracle(next_x)
 
-        while not self.is_stopping_critiria(fx, f_next_x):
-            fx = self.zero_order_oracle(next_x)
-            next_x = self.compute_next_x(next_x)
+        while not self.is_stopping_criteria(fx, f_next_x):
+            fx = f_next_x
+            t += 1
+            next_x = self.compute_next_x(next_x, t)
             f_next_x = self.zero_order_oracle(next_x)
 
         return next_x, f_next_x
@@ -52,19 +54,102 @@ class GradientDecent:
 
 
 class NonSmoothPGD(GradientDecent):
-    def __init__(self, A: np.ndarray, b: np.array, x_0: np.array, epsilon: float, R:float):
-        super().__init__(A, b, x_0, epsilon)
-        
+    def __init__(self, A: np.ndarray, b: np.array, x_1: np.array, epsilon: float, R:float, A_sigma_max: float):
+        super().__init__(A, b, x_1, epsilon)
+        self.R = R
+        self.A_sigma_max = A_sigma_max
+        self.L = self.calculate_lipschitz()
+        self.x_values = [x_1]
+
+    def calculate_lipschitz(self):
+        """
+        Calculate L-Lipschitz parameter of LR function by: sigma_max(A)^2*R + sigma_max(A)*||b||
+        :return:
+        """
+        return self.A_sigma_max**2*self.R + self.A_sigma_max*np.linalg.norm(self.b, ord=2)
 
     @property
     def step_size(self, *args):
-        return self.R
+        t = args[0]
+        return self.R/(self.L*np.sqrt(t))
 
     def compute_next_x(self, *args):
-        ...
+        x = args[0]
+        t = args[1]
+
+        next_y = x - self.step_size(t)*self.first_order_oracle(x)
+        next_x = self.calculate_projection(next_y)
+        self.x_values.append(next_x)
+        return next_x
 
     def is_stopping_criteria(self, fx, f_next_x):
-        ...
+        x_values_t_minus_1 = deepcopy(self.x_values).pop()
 
-    def calculate_projection(self):
-        ...
+        x_values_t_minus_1_avg = np.mean(x_values_t_minus_1)
+        x_values_avg = np.mean(self.x_values)
+
+        fx = self.zero_order_oracle(x_values_t_minus_1_avg)
+        f_next_x = self.zero_order_oracle(x_values_avg)
+
+        if f_next_x - fx <= self.epsilon:
+            return True
+        else:
+            return False
+
+    def calculate_projection(self, x):
+        nominator = self.R * x
+        denominator = np.amax((np.linalg.norm(x, 2), self.R))
+        return nominator/float(denominator)
+
+
+class SmoothGD(GradientDecent):
+    def __init__(self, A: np.ndarray, b: np.array, x_1: np.array, epsilon: float, beta: float):
+        super().__init__(A, b, x_1, epsilon)
+        self.beta = beta
+
+    def step_size(self, *args):
+        return 1/self.beta
+
+    def compute_next_x(self, *args):
+        x = args[0]
+        gradient = self.first_order_oracle(x)
+        next_x = x - self.step_size()*gradient
+        return next_x
+
+    def is_stopping_criteria(self, fx, f_next_x):
+        if f_next_x - fx < self.epsilon:
+            return True
+        else:
+            return False
+
+
+class AcceleratedGD(GradientDecent):
+    def __init__(self, A: np.ndarray, b: np.array, x_1: np.array, epsilon: float, alpha: float, beta: float):
+        super().__init__(A, b, x_1, epsilon)
+        self.alpha = alpha
+        self.beta = beta
+        self.k = self.beta/self.alpha
+        self.y = x_1
+
+    def step_size(self, *args):
+        return 1/self.beta
+
+    def compute_next_x(self, *args):
+        x = args[0]
+        gradient = self.first_order_oracle(x)
+        next_y = x - self.step_size()*gradient
+        
+        sqrt_k = np.sqrt(self.k)
+        lhs = (1 + (sqrt_k  - 1)/(sqrt_k + 1))*next_y
+        rhs = ((sqrt_k - 1)/(sqrt_k + 1))*self.y
+
+        next_x = lhs - rhs
+        self.y = next_y
+
+        return next_x
+
+    def is_stopping_criteria(self, fx, f_next_x):
+        if f_next_x - fx < self.epsilon:
+            return True
+        else:
+            return False
